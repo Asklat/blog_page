694b6b74c0b7c38de216175cc034c6b32d5c6ff7
Asklat <alejandro.gamaza.martinez.vega@gmail.com>
2021-02-24 16:11:46 +0100

    Añade formulario para crear cuentas de usuario

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('blog.urls')),
 ]
